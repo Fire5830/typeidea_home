383413d7fd8af85559dabd1e129790c2b7122d9d
Fire5830 <18665871578@163.com>
2021-09-17 11:06:07 +0800

    update

---
+++ typeidea/venv/Scripts/manage.py
@@ -0,0 +1,7 @@
+#!E:\study\project\typeidea\venv\Scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'typeidea==0.1','manage.py'
+__requires__ = 'typeidea==0.1'
+__import__('pkg_resources').require('typeidea==0.1')
+__file__ = 'E:\\study\\project\\typeidea\\manage.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
